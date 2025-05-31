3fb5273a9d185601a5109e5c540c8568f50f8c80
haanhvu <haanh6594@gmail.com>
2025-05-31 17:19:38 +0700

    Delete unused firebase function

--- firebase/functions/index.js
+++
@@ -1,7 +0,0 @@
-const functions = require("firebase-functions");
-
-const apiKey = functions.config().api.key;
-
-exports.getApiKey = functions.https.onCall(async (data, context) => {
-  return apiKey;
-});
